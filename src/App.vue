1b8c987d748065a47d2389ce8c7327e556fe1fbd
otouto <otoutoyeah@gmail.com>
2019-10-30 14:32:17 +0900

    add header design

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <div id="app">
-    <!---
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    --->
     <HeaderItem/>
     <router-view/>
   </div>
@@ -31,16 +25,4 @@ export default {
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
