5122878e2c2978597d9dc5f4aa8e8af9eadeca70
otouto <otoutoyeah@gmail.com>
2019-11-01 16:27:51 +0900

    update-contact-design

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,4 @@ export default {
 
 <style scoped>
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #0d9e66;
-}
-
 </style>
