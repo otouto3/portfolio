5b768bf99e2d3e75967b174d91f81cd8dfbb3a2c
otouto3 <otoutoyeah@gmail.com>
2020-06-04 20:06:43 +0900

    インデントの改善

--- src/components/WorkCard.vue
+++ src/components/WorkCard.vue
@@ -23,27 +23,23 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 <template>
   <section class="work-card">
-    <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/devicons/1.8.0/css/devicons.min.css" rel="stylesheet">
-    <img class="work-img" v-bind:src="item.img_path" width="300" height ="300" alt="" />
+    <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/devicons/1.8.0/css/devicons.min.css" rel="stylesheet" />
+    <img class="work-img" v-bind:src="item.img_path" width="300" height="300" alt />
     <div class="work-content">
-      <p class="work-title">
-        {{ item.title }}
-      </p>
-      <p class="work-text">
-        {{ item.text }}
-      </p>
+      <p class="work-title">{{ item.title }}</p>
+      <p class="work-text">{{ item.text }}</p>
       <div class="work-icon">
         <ul>
-          <li v-for="(icon, index) in item.font_awesome" :key=index>
-            <font-awesome-icon :icon="icon.value"/>
+          <li v-for="(icon, index) in item.font_awesome" :key="index">
+            <font-awesome-icon :icon="icon.value" />
           </li>
-          <li v-for="(icon, index) in item.devicon" :key=index>
+          <li v-for="(icon, index) in item.devicon" :key="index">
             <i :class="icon.value"></i>
           </li>
         </ul>
       </div>
-      <div class="work-link" v-for="(link, index) in item.links" :key=index>
+      <div class="work-link" v-for="(link, index) in item.links" :key="index">
         <a :href="link.link">{{ link.link_name }}</a>
       </div>
     </div>
@@ -52,21 +48,20 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 <script>
 export default {
-  name: 'WorkCard',
+  name: "WorkCard",
   props: {
     item: {
       type: Object,
       default: () => {}
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
 .work-card {
   width: 300px;
-  padding:  20px;
+  padding: 20px;
   margin: 10px;
   background: rgb(235, 233, 233);
   border-radius: 10px;
@@ -79,87 +74,77 @@ export default {
 .work-content {
   padding: 20px;
 
-    .work-img {
-      margin-top: 3px;
-      border-radius: 5px 5px 0 0;
-      width: 300px;
-      height: 300px;
+  .work-img {
+    margin-top: 3px;
+    border-radius: 5px 5px 0 0;
+    width: 300px;
+    height: 300px;
+  }
 
-    }
+  .work-title {
+    font-family: "Nico Moji";
+    font-size: 20px;
+    margin-bottom: 20px;
+    text-align: center;
+    color: #333;
 
-    .work-title {
-      font-family: "Nico Moji";
-      font-size: 20px;
-      margin-bottom: 20px;
+    margin-top: 0px;
+  }
+
+  .work-text {
+    color: #777;
+    font-size: 14px;
+    line-height: 1.5;
+  }
+
+  .work-icon {
+    ul {
+      padding: 0;
       text-align: center;
-      color: #333;
-
-      margin-top: 0px;
     }
 
-    .work-text {
-      color: #777;
-      font-size: 14px;
-      line-height: 1.5;
+    li {
+      display: inline-block;
+      font-size: 2.5em;
+      padding: 0 10px;
     }
+  }
 
-    .work-icon {
+  .work-link {
+    text-align: center;
+    border-top: 1px solid #eee;
+    padding: 20px;
 
-      ul {
-        padding: 0;
-        text-align: center;
-      }
+    a {
+      text-decoration: none;
+      color: rgb(224, 93, 174);
+      margin: 0 10px;
 
-      li {
-        display: inline-block;
-        font-size: 2.5em;
-        padding: 0 10px;
-      }
-    }
-
-    .work-link {
-
-      text-align: center;
-      border-top: 1px solid #eee;
-      padding: 20px;
-
-      a {
-        text-decoration: none;
-        color: rgb(224, 93, 174);
-        margin: 0 10px;
-
-        &:hover {
-          color: #0090aa;
-        }
+      &:hover {
+        color: #0090aa;
       }
     }
-
+  }
 }
 
-
 /* iphone6,7,8 */
-@media (min-width: 413px) and (max-width: 420px){
-    .work-card {
-        margin: 28px;
-    }
+@media (min-width: 413px) and (max-width: 420px) {
+  .work-card {
+    margin: 28px;
+  }
 }
 
 /* ipad */
-@media (min-width: 767px) and (max-width: 769px){
-    .work-card {
-        margin-right: 24px;
-    }
+@media (min-width: 767px) and (max-width: 769px) {
+  .work-card {
+    margin-right: 24px;
+  }
 }
 
 /* ipad pro / pc用 */
-@media (min-width: 770px){
-    .work-card {
-        margin-left: 50px;
-    }
+@media (min-width: 770px) {
+  .work-card {
+    margin-left: 50px;
+  }
 }
-
-
-
-
-
 </style>
